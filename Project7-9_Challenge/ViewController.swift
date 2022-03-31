9e12e30f75b8e5ea103a206620142e72444e76b1
Oscarlui <oscarlu1@connect.hku.hk>
2022-03-31 23:45:47 +0800

    Initial Commit

---
+++ Project7-9_Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project7-9_Challenge
+//
+//  Created by Oscar Lui on 31/3/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
